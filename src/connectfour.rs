5c47943e943d64c83c0fee1579fa53733de12c0f
Cedric Decoster <cedric.decoster@gmail.com>
2021-12-18 13:39:07 +0100

    updated connectfour

--- src/connectfour.rs
+++ src/connectfour.rs
@@ -1,77 +1,79 @@
 // use rand::Rng;
 
-pub struct Logic {
-}
+pub struct Logic {}
 
 impl Logic {
-
     pub fn full(board: [[u8; 6]; 7]) -> bool {
         let y_pos = board[0].len() - 1;
         for x_pos in 0..board.len() {
             if board[x_pos][y_pos] == 0 {
-                return false
+                return false;
             }
         }
-        
+
         true
     }
 
     pub fn evaluate(board: [[u8; 6]; 7], player: u8) -> bool {
-        // horizontalCheck 
+        // horizontalCheck
         for y in 0..board[0].len() {
-            for x in 0..board.len()-3 {
-                if board[x][y] == player 
-                && board[x+1][y] == player 
-                && board[x+2][y] == player 
-                && board[x+3][y] == player {
-                    return true;
-                }           
-            }
-        }
-        
-        // verticalCheck
-        for y in 0..board[0].len()-3 {
-            for x in 0..board.len() {
-                if board[x][y] == player 
-                && board[x][y+1] == player
-                && board[x][y+2] == player
-                && board[x][y+3] == player {
-                    return true;
-                }           
-            }
-        }
-        
-        // ascendingDiagonalCheck 
-        for y in 0..board[0].len()-3 {
-            for x in 3..board.len() {
-                if board[x][y] == player 
-                && board[x-1][y+1] == player
-                && board[x-2][y+2] == player
-                && board[x-3][y+3] == player {
+            for x in 0..board.len() - 3 {
+                if board[x][y] == player
+                    && board[x + 1][y] == player
+                    && board[x + 2][y] == player
+                    && board[x + 3][y] == player
+                {
                     return true;
                 }
             }
         }
 
-        // descendingDiagonalCheck 
+        // verticalCheck
+        for y in 0..board[0].len() - 3 {
+            for x in 0..board.len() {
+                if board[x][y] == player
+                    && board[x][y + 1] == player
+                    && board[x][y + 2] == player
+                    && board[x][y + 3] == player
+                {
+                    return true;
+                }
+            }
+        }
+
+        // ascendingDiagonalCheck
+        for y in 0..board[0].len() - 3 {
+            for x in 3..board.len() {
+                if board[x][y] == player
+                    && board[x - 1][y + 1] == player
+                    && board[x - 2][y + 2] == player
+                    && board[x - 3][y + 3] == player
+                {
+                    return true;
+                }
+            }
+        }
+
+        // descendingDiagonalCheck
         for y in 3..board[0].len() {
             for x in 3..board.len() {
-                if board[x][y] == player 
-                && board[x-1][y-1] == player
-                && board[x-2][y-2] == player
-                && board[x-3][y-3] == player {
+                if board[x][y] == player
+                    && board[x - 1][y - 1] == player
+                    && board[x - 2][y - 2] == player
+                    && board[x - 3][y - 3] == player
+                {
                     return true;
                 }
             }
         }
         return false;
     }
-    
+
     pub fn add_stone(board: &mut [[u8; 6]; 7], column: u8, player: u8) -> bool {
         if board[column as usize][0] > 0 {
             return false;
         }
-        let board_rows:usize = board[0].len();
+        let board_rows: usize = board[0].len();
         for y in 0..board_rows {
             let y_pos = board_rows - y - 1;
             if board[column as usize][y_pos] > 0 {
@@ -82,13 +84,13 @@ impl Logic {
         }
         true
     }
-    
+
     // pub fn random_board() ->  [[u8; 6]; 7] {
     //     let mut board = [[0u8; 6]; 7];
     //     let mut rng = rand::thread_rng();
     //     let mut n1: u8 = rng.gen();
     //     n1 = n1 % 42;
-    //     // add randomly stones to the board   
+    //     // add randomly stones to the board
     //     for i in 0..n1 {
     //         let mut stone_set = true;
     //         loop {
@@ -101,7 +103,7 @@ impl Logic {
     //     }
     //     board
     // }
-    
+
     // pub fn print_board(board: [[u8; 6]; 7]) {
     //     println!("   c0  c1  c2  c3  c4  c5  c6  ");
     //     println!("  + - + - + - + - + - + - + - +");
@@ -119,11 +121,10 @@ impl Logic {
     //         println!("  + - + - + - + - + - + - + - +");
     //     }
     // }
-
 }
 
 // fn main() {
-    
+
 //     loop {
 //         let board = Logic::random_board();
 //         if Logic::evaluate(board, 1) || Logic::evaluate(board, 2) {
--- src/tests.rs
+++ src/tests.rs
@@ -1,123 +1,184 @@
 use super::*;
-use crate::{Error, mock::*};
+use crate::{mock::*, Error};
 
-use frame_support::{assert_ok, assert_noop};
+use frame_support::{assert_noop, assert_ok};
 
 #[test]
 fn test_game_creation() {
-	new_test_ext().execute_with(|| {
+    new_test_ext().execute_with(|| {
+        // Test player can not play against himself
+        assert_noop!(
+            ConnectFour::new_game(Origin::signed(1), 1),
+            Error::<Test>::NoFakePlay
+        );
 
-		// Test player can not play against himself
-		assert_noop!(
-			ConnectFour::new_game(Origin::signed(1), 1),
-			Error::<Test>::NoFakePlay
-		);
+        // Test game creation between to different players
+        assert_ok!(ConnectFour::new_game(Origin::signed(1), 2));
+        run_to_block(1);
 
-		// Test game creation between to different players
-		assert_ok!(ConnectFour::new_game(Origin::signed(1), 2));
-		run_to_block(1);
+        let board_id_1 = ConnectFour::player_board(1);
+        let board_id_2 = ConnectFour::player_board(2);
 
-		let board_id_1 = ConnectFour::player_board(1);
-		let board_id_2 = ConnectFour::player_board(2);
+        assert_eq!(board_id_1, board_id_2);
 
-		assert_eq!(board_id_1, board_id_2);
+        assert_noop!(
+            ConnectFour::new_game(Origin::signed(1), 3),
+            Error::<Test>::PlayerBoardExists
+        );
 
-		assert_noop!(
-			ConnectFour::new_game(Origin::signed(1), 3),
-			Error::<Test>::PlayerBoardExists
-		);
+        assert_noop!(
+            ConnectFour::new_game(Origin::signed(3), 2),
+            Error::<Test>::PlayerBoardExists
+        );
 
-		assert_noop!(
-			ConnectFour::new_game(Origin::signed(3), 2),
-			Error::<Test>::PlayerBoardExists
-		);
+        let board = ConnectFour::boards(board_id_1).unwrap();
 
-		let board = ConnectFour::boards(board_id_1);
-
-		assert_eq!(board.last_turn, 0);
-
-	});
+        assert_eq!(board.last_turn, 0);
+    });
 }
 
 #[test]
 fn test_game_play() {
-	new_test_ext().execute_with(|| {
+    new_test_ext().execute_with(|| {
+        let mut current_block: u64 = 100;
 
-		let mut current_block:u64 = 100;
+        // start from block 100
+        run_to_block(current_block);
 
-		// start from block 100
-		run_to_block(current_block);
+        // Test game creation between to different players
+        assert_ok!(ConnectFour::new_game(
+            Origin::signed(PLAYER_1 as u64),
+            PLAYER_2 as u64
+        ));
+        let board_id = ConnectFour::player_board(PLAYER_1 as u64);
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert_eq!(board.last_turn, current_block);
 
-		// Test game creation between to different players
-		assert_ok!(ConnectFour::new_game(Origin::signed(PLAYER_1 as u64), PLAYER_2 as u64));
-		let board_id = ConnectFour::player_board(PLAYER_1 as u64);
-		let board = ConnectFour::boards(board_id);
-		assert_eq!(board.last_turn, current_block);
+        run_next_block();
+        current_block = current_block + 1;
 
-		run_next_block();
-		current_block = current_block + 1;
+        assert_eq!(System::block_number(), current_block);
 
-		assert_eq!(System::block_number(), current_block);
+        if board.next_player == PLAYER_1 {
+            assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 0));
+            let board = ConnectFour::boards(board_id).unwrap();
+            assert!(board.board_state == BoardState::Running);
+            assert!(board.next_player == PLAYER_2);
+            assert_eq!(board.last_turn, current_block);
 
-		if board.next_player == PLAYER_1 {
-			assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 0));
-			let board = ConnectFour::boards(board_id);
-			assert!(board.board_state == BoardState::Running);	
-			assert!(board.next_player == PLAYER_2);
-			assert_eq!(board.last_turn, current_block);
+            run_next_block();
+            current_block = current_block + 1;
+        }
 
-			run_next_block();
-			current_block = current_block + 1;
-		}
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert_eq!(board.last_turn, current_block);
+        assert!(board.board_state == BoardState::Running);
+        assert!(board.next_player == PLAYER_1);
 
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
-		let board = ConnectFour::boards(board_id);
-		assert_eq!(board.last_turn, current_block);
-		assert!(board.board_state == BoardState::Running);
-		assert!(board.next_player == PLAYER_1);
+        run_next_block();
+        current_block = current_block + 1;
 
-		run_next_block();
-		current_block = current_block + 1;
-		
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 2));
-		let board = ConnectFour::boards(board_id);
-		assert!(board.board_state == BoardState::Running);
-		
-		run_next_block();
-		current_block = current_block + 1;
-		
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
-		let board = ConnectFour::boards(board_id);
-		assert!(board.board_state == BoardState::Running);
-		
-		run_next_block();
-		current_block = current_block + 1;
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 2));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert!(board.board_state == BoardState::Running);
 
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 3));
-		let board = ConnectFour::boards(board_id);
-		assert!(board.board_state == BoardState::Running);
-		
-		run_next_block();
-		current_block = current_block + 1;
-		
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
-		let board = ConnectFour::boards(board_id);
-		assert!(board.board_state == BoardState::Running);
-		
-		run_next_block();
-		current_block = current_block + 1;
-		
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 4));
-		let board = ConnectFour::boards(board_id);
-		assert!(board.board_state == BoardState::Running);
-		
-		run_next_block();
-		current_block = current_block + 1;
-		
-		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
-		let board = ConnectFour::boards(board_id);
-		assert!(board.board_state == BoardState::Finished(board.blue));
-		assert_eq!(board.last_turn, current_block);
+        run_next_block();
+        current_block = current_block + 1;
 
-	});
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert!(board.board_state == BoardState::Running);
+
+        run_next_block();
+        current_block = current_block + 1;
+
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 3));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert!(board.board_state == BoardState::Running);
+
+        run_next_block();
+        current_block = current_block + 1;
+
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert!(board.board_state == BoardState::Running);
+
+        run_next_block();
+        current_block = current_block + 1;
+
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 4));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert!(board.board_state == BoardState::Running);
+
+        run_next_block();
+        current_block = current_block + 1;
+
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert!(board.board_state == BoardState::Finished(Some(board.blue)));
+        assert_eq!(board.last_turn, current_block);
+    });
+}
+
+#[test]
+fn test_force_turn() {
+    new_test_ext().execute_with(|| {
+        let mut current_block: u64 = 100;
+
+        // start from block 100
+        run_to_block(current_block);
+
+        // Test game creation between to different players
+        assert_ok!(ConnectFour::new_game(
+            Origin::signed(PLAYER_1 as u64),
+            PLAYER_2 as u64
+        ));
+        let board_id = ConnectFour::player_board(PLAYER_1 as u64);
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert_eq!(board.last_turn, current_block);
+
+        run_next_block();
+        current_block = current_block + 1;
+
+        assert_eq!(System::block_number(), current_block);
+
+        if board.next_player == PLAYER_1 {
+            assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 0));
+            let board = ConnectFour::boards(board_id).unwrap();
+            assert!(board.board_state == BoardState::Running);
+            assert!(board.next_player == PLAYER_2);
+            assert_eq!(board.last_turn, current_block);
+
+            run_next_block();
+            current_block = current_block + 1;
+        }
+
+        assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert_eq!(board.last_turn, current_block);
+        assert!(board.board_state == BoardState::Running);
+        assert!(board.next_player == PLAYER_1);
+
+        run_to_block(current_block + 10);
+        current_block = current_block + 10;
+
+        // check if force turn ended the game
+        let board = ConnectFour::boards(board_id).unwrap();
+        assert_eq!(board.last_turn, current_block);
+        assert!(board.board_state == BoardState::Finished(Some(board.blue)));
+
+        assert!(Boards::<Test>::contains_key(board_id));
+        assert!(PlayerBoard::<Test>::contains_key(board.red));
+        assert!(PlayerBoard::<Test>::contains_key(board.blue));
+        assert!(BoardSchedules::<Test>::contains_key(board_id));
+
+        run_to_block(current_block + 20);
+
+        // check if boards are cleaned up
+        assert!(!Boards::<Test>::contains_key(board_id));
+        assert!(!PlayerBoard::<Test>::contains_key(board.red));
+        assert!(!PlayerBoard::<Test>::contains_key(board.blue));
+        assert!(!BoardSchedules::<Test>::contains_key(board_id));
+    });
 }
